ed19db21e61bc66cb9d24da4e0895e73092eaeed
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-03-08 11:08:51 +0530

    crate controllers

---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const express = require('express');
+
+
+const productController=require('../controllers/product')
+
+const router = express.Router();
+
+
+// /admin/add-product => GET
+router.get('/add-product',productController.getAddProduct)
+
+// /admin/add-product => POST
+router.post('/add-product',productController.postAddProduct)
+
+module.exports = router;
---
+++ routes/shop.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const express = require('express');
+
+const productController=require('../controllers/product')
+
+const router = express.Router();
+
+router.get('/', productController.getProducts)
+
+
+module.exports = router;
